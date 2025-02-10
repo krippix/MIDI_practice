1d6b7f15911f3db7144aaa70c720d663bc48281a
krippix <krippix@users.noreply.github.com>
2025-02-10 22:01:36 +0100

    Added MIDI selector and changed settings menu

--- html/assets/scripts/midi.js
+++ html/assets/scripts/midi.js
@@ -1,12 +1,14 @@
 // prepare svg object for later use
-var keyboard = document.getElementById("piano");
+var keyboard;
 var svgDoc; // loaded keyboard svg
 var keyArray; // List of every single key
 var midiAccess = null;
+var inputList = []; // List of all MIDI devices
 const black_keys = [2,5,7,10,12,14,17,19,22,24,26,29,31,34,36,38,41,43,46,48,50,53,55,58,60,62,65,67,70,72,74,77,79,82,84,86] 
 
 
 window.addEventListener("load", () => {
+    keyboard = document.getElementById("piano");
     svgDoc = keyboard.contentDocument;
     keyArray = svgDoc.querySelectorAll('.key');
     keyArray.forEach(key => {
@@ -56,19 +58,21 @@ function midiSetup() {
  */
 function onMIDISuccess(access) {
     midiAccess = access;
-    console.log("access successful");
+    console.log("MIDI access successful.");
 
-    var input_list = [];
+    let html_list = document.getElementById("midi-device-select");
 
     var iter = midiAccess.inputs.values();
     for (let obj = iter.next(); !obj.done; obj = iter.next()) {
-        input_list.push(obj.value);
+        inputList.push(obj.value);
+        html_list.innerHTML = html_list.innerHTML + '<option value="'+(inputList.length - 1)+'">'+obj.value.name+'</option>'
     }
 
     // Add event listener do all devices
-    for (var i = 0; i < input_list.length; i++) {
-        input_list[i].onmidimessage = getMIDIMessage;
+    for (var i = 0; i < inputList.length; i++) {
+        inputList[i].onmidimessage = getMIDIMessage;
     }
+    // TODO: find better way to differentiate between midi devices and remember selection
 }
 
 
@@ -77,8 +81,25 @@ function onMIDISuccess(access) {
  */
 function onMIDIFailure() {
     let message = "Failed to access your MIDI device";
-    console.log(message);
-    alert(message);
+    console.error(message);
+}
+
+
+/**
+ * Handle MIDI device change
+ */
+function onDeviceChange() {
+    let selection = document.getElementById("midi-device-select").value;
+    
+    // If ALL is selected attach handler to all insead of clearing them
+    var func = selection == 'null' ? getMIDIMessage : function (){};
+
+    inputList.forEach((x, i) => {
+        x.onmidimessage = func;
+    })
+
+    if (selection == 'null') return;
+    inputList[selection].onmidimessage = getMIDIMessage;
 }
 
 
@@ -90,8 +111,6 @@ function getMIDIMessage(midiMessage) {
     var command  = midiMessage.data[0];
     var note     = midiMessage.data[1] - 20; // sorry midi standard
     var velocity = (midiMessage.data.length > 2) ? midiMessage.data[2] : 0;
-
-    console.log(`MIDI{ command : ${command}, note : ${note}, velocity : ${velocity}}`);
 
     if (command == 144){
         console.log(note);
